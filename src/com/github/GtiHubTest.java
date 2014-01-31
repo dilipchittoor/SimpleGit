d5dcc81f764d13bbccbc1338b1a990fba8dce1c6
chittoordilip <chittoordilip@gmail.com>
2014-01-31 11:21:59 +0530

    Simplegit

---
+++ src/com/github/GtiHubTest.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class GtiHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Getting it");
+	}
+
+}
